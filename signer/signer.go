d1e0212d67ea2c781ae1f5bb3c20005b982ea5b4
Luis Osta <luisosta1999@gmail.com>
2022-04-22 09:50:46 -0700

    added string names for Verification Relations

--- signer/signer.go
+++ signer/signer.go
@@ -11,6 +11,23 @@ const (
 	AssertionMethod
 )
 
+func (vr VerificationRelation) String() string {
+	switch vr {
+	case Master:
+		return "master"
+	case Authentication:
+		return "authentication"
+	case CapabilityInvocation:
+		return "capabilityInvocation"
+	case CapabilityDelegation:
+		return "capabilityDelegation"
+	case AssertionMethod:
+		return "associationMethod"
+	default:
+		return ""
+	}
+}
+
 type DynamicSigner interface {
 	Sign(rel VerificationRelation, message []byte) ([]byte, error)
 	GetDid() string
