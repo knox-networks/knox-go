d38b380f6535d3256e4c6d03a38db2dbcfa5a053
Luis Osta <luisosta1999@gmail.com>
2022-04-27 13:39:48 -0700

    added test coverage for `Presentation.Request`

--- presentation/presentation_test.go
+++ presentation/presentation_test.go
@@ -1,6 +1,8 @@
 package presentation
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -11,7 +13,7 @@ import (
 	s_mock "github.com/knox-networks/knox-go/signer/mock"
 )
 
-func TestPresentCredential(t *testing.T) {
+func TestSharePresentation(t *testing.T) {
 	cred_types := []string{"PermanentResidentCard"}
 	cred := []byte(`{
 		"@context":[
@@ -76,4 +78,88 @@ func TestPresentCredential(t *testing.T) {
 		t.Errorf("Expected no error, got %v", err)
 	}
 
+}
+
+type requestPresentationFields struct {
+	ca credential_adapter.CredentialAdapterClient
+}
+type requestPresentationArgs struct {
+	credTypes []string
+	nonce     string
+}
+
+type requestPresentationTest struct {
+	name          string
+	prepare       func(f *requestPresentationFields, args *requestPresentationArgs)
+	expectedError error
+	args          *requestPresentationArgs
+}
+
+func TestRequestPresentation(t *testing.T) {
+	mock_controller := gomock.NewController(t)
+	f := &requestPresentationFields{
+		ca: ca_mock.NewMockCredentialAdapterClient(mock_controller),
+	}
+
+	tests := []requestPresentationTest{
+		{
+			name:          "RequestPresentation Succeeds",
+			expectedError: nil,
+			prepare: func(f *requestPresentationFields, args *requestPresentationArgs) {
+				gomock.InOrder(
+					f.ca.(*ca_mock.MockCredentialAdapterClient).EXPECT().
+						CreatePresentationChallenge(args.credTypes).
+						Return(&credential_adapter.PresentationChallenge{
+							Nonce:           args.nonce,
+							CredentialTypes: args.credTypes,
+						}, nil),
+				)
+			},
+			args: &requestPresentationArgs{
+				credTypes: []string{"PermanentResidentCard"},
+			},
+		},
+		{
+			name:          "RequestPresentation Fails Due To CreatePresentationChallenge Error",
+			expectedError: errors.New("CreatePresentationChallenge Error"),
+			prepare: func(f *requestPresentationFields, args *requestPresentationArgs) {
+				gomock.InOrder(
+					f.ca.(*ca_mock.MockCredentialAdapterClient).EXPECT().
+						CreatePresentationChallenge(args.credTypes).
+						Return(&credential_adapter.PresentationChallenge{}, errors.New("CreatePresentationChallenge Error")),
+				)
+			},
+			args: &requestPresentationArgs{
+				credTypes: []string{"PermanentResidentCard"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			test.prepare(f, test.args)
+			pc := &presentationClient{ca: f.ca}
+			challenge, err := pc.Request(params.RequestPresentationParams{
+				CredentialTypes: test.args.credTypes,
+			})
+			if (err == nil && test.expectedError != nil) || (err != nil && test.expectedError == nil) {
+				t.Errorf("Expected error %v, got %v", test.expectedError, err)
+			}
+
+			if err != nil && test.expectedError != nil {
+				if err.Error() != test.expectedError.Error() {
+					t.Errorf("Expected error %v, got %v", test.expectedError, err)
+				}
+			}
+
+			if test.expectedError == nil && challenge.Nonce != test.args.nonce {
+				t.Errorf("Expected nonce %s, got %s", test.args.nonce, challenge.Nonce)
+			}
+
+			if test.expectedError == nil && !reflect.DeepEqual(challenge.CredentialTypes, test.args.credTypes) {
+				t.Errorf("Expected credential types %v, got %v", test.args.credTypes, challenge.CredentialTypes)
+			}
+		})
+	}
 }
