c131cadbc53b050195b770e97c04450aa0d286c3
Luis Osta <luisosta1999@gmail.com>
2022-05-03 12:49:21 -0700

    Added TestNewTokenClient test

--- token/token_test.go
+++ token/token_test.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -81,6 +82,164 @@ func TestCreateToken(t *testing.T) {
 			},
 			expectedError: nil,
 		},
+		{
+			name: "CreateToken With Did Authentication Fails Creating Challenge",
+			prepare: func(f *createTokenFields, args *createTokenArgs) {
+
+				gomock.InOrder(
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						CreateDidAuthenticationChallenge().
+						Return(&auth_client.DidAuthenticationChallenge{}, f.authStream, errors.New("challenge error")),
+				)
+			},
+			args: createTokenArgs{
+				p: &params.CreateTokenParams{
+					Did: &params.DidAuthentication{
+						Did: "did:knox:test",
+					},
+				},
+			},
+			expectedError: errors.New("challenge error"),
+		},
+		{
+			name: "CreateToken With Did Authentication Fails Creating Signature",
+			prepare: func(f *createTokenFields, args *createTokenArgs) {
+				message := []byte(args.p.Did.Did + "." + args.nonce)
+				gomock.InOrder(
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						CreateDidAuthenticationChallenge().
+						Return(&auth_client.DidAuthenticationChallenge{Nonce: args.nonce}, f.authStream, nil),
+					f.signer.(*s_mock.MockDynamicSigner).EXPECT().
+						Sign(signer.Authentication, message).
+						Return([]byte(""), errors.New("signature error")),
+				)
+			},
+			args: createTokenArgs{
+				p: &params.CreateTokenParams{
+					Did: &params.DidAuthentication{
+						Did: "did:knox:test",
+					},
+				},
+				nonce: "nonce",
+			},
+			expectedError: errors.New("signature error"),
+		},
+		{
+			name: "CreateToken With Did Authentication Fails Authenticating Against Service",
+			prepare: func(f *createTokenFields, args *createTokenArgs) {
+				signature := []byte("signature")
+				message := []byte(args.p.Did.Did + "." + args.nonce)
+				gomock.InOrder(
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						CreateDidAuthenticationChallenge().
+						Return(&auth_client.DidAuthenticationChallenge{Nonce: args.nonce}, f.authStream, nil),
+					f.signer.(*s_mock.MockDynamicSigner).EXPECT().
+						Sign(signer.Authentication, message).
+						Return(signature, nil),
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						AuthnWithDid(args.p.Did.Did, args.nonce, signature).
+						Return(errors.New("authentication error")),
+				)
+			},
+			args: createTokenArgs{
+				p: &params.CreateTokenParams{
+					Did: &params.DidAuthentication{
+						Did: "did:knox:test",
+					},
+				},
+				nonce: "nonce",
+			},
+			expectedError: errors.New("authentication error"),
+		},
+		{
+			name: "CreateToken With Did Authentication Fails To Receive Message",
+			prepare: func(f *createTokenFields, args *createTokenArgs) {
+				signature := []byte("signature")
+				message := []byte(args.p.Did.Did + "." + args.nonce)
+				gomock.InOrder(
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						CreateDidAuthenticationChallenge().
+						Return(&auth_client.DidAuthenticationChallenge{
+							Nonce: args.nonce,
+						}, f.authStream, nil),
+					f.signer.(*s_mock.MockDynamicSigner).EXPECT().
+						Sign(signer.Authentication, message).
+						Return(signature, nil),
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						AuthnWithDid(args.p.Did.Did, args.nonce, signature).
+						Return(nil),
+					f.authStream.(*grpc_auth_mock.MockAuthApiService_AuthnWithDidStartClient).
+						EXPECT().Recv().Return(&AuthApi.AuthnWithDidStartResponse{}, errors.New("receive error")),
+				)
+			},
+			args: createTokenArgs{
+				p: &params.CreateTokenParams{
+					Did: &params.DidAuthentication{
+						Did: "did:knox:test",
+					},
+				},
+				nonce: "nonce",
+			},
+			expectedError: errors.New("receive error"),
+		},
+		{
+			name: "CreateToken With Did Authentication Fails To Close Stream",
+			prepare: func(f *createTokenFields, args *createTokenArgs) {
+				signature := []byte("signature")
+				message := []byte(args.p.Did.Did + "." + args.nonce)
+				gomock.InOrder(
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						CreateDidAuthenticationChallenge().
+						Return(&auth_client.DidAuthenticationChallenge{
+							Nonce: args.nonce,
+						}, f.authStream, nil),
+					f.signer.(*s_mock.MockDynamicSigner).EXPECT().
+						Sign(signer.Authentication, message).
+						Return(signature, nil),
+					f.auth.(*auth_mock.MockAuthClient).
+						EXPECT().
+						AuthnWithDid(args.p.Did.Did, args.nonce, signature).
+						Return(nil),
+					f.authStream.(*grpc_auth_mock.MockAuthApiService_AuthnWithDidStartClient).
+						EXPECT().Recv().Return(&AuthApi.AuthnWithDidStartResponse{
+						DidStart: &AuthApi.AuthnWithDidStartResponse_AuthToken{
+							AuthToken: &AuthApi.AuthTokenResponse{
+								AuthToken: &AuthApi.AuthToken{
+									Token: "token",
+								},
+							},
+						},
+					}, nil),
+					f.authStream.(*grpc_auth_mock.MockAuthApiService_AuthnWithDidStartClient).
+						EXPECT().CloseSend().Return(errors.New("close error")),
+				)
+			},
+			args: createTokenArgs{
+				p: &params.CreateTokenParams{
+					Did: &params.DidAuthentication{
+						Did: "did:knox:test",
+					},
+				},
+				nonce: "nonce",
+			},
+			expectedError: errors.New("close error"),
+		},
+		{
+			name: "CreateToken Fails Due To Neither Did Nor Password Authentication Provided",
+			prepare: func(f *createTokenFields, args *createTokenArgs) {
+			},
+			args: createTokenArgs{
+				p: &params.CreateTokenParams{},
+			},
+			expectedError: errors.New("no authentication method specified"),
+		},
 	}
 
 	for _, test := range tests {
@@ -102,5 +261,17 @@ func TestCreateToken(t *testing.T) {
 				t.Errorf("Expected error: %v, got: %v", test.expectedError, err)
 			}
 		})
+	}
+}
+
+func TestNewTokenClient(t *testing.T) {
+	mockController := gomock.NewController(t)
+
+	signer := s_mock.NewMockDynamicSigner(mockController)
+
+	_, err := NewTokenClient("", signer)
+
+	if err != nil {
+		t.Errorf("Expected no error, got: %v", err)
 	}
 }
